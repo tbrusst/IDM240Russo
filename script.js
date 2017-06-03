28f1465a8d7d62de9591517f013dd062a8229749
BrendanRusso <BrendanRusso@Brendans-MacBook-Pro-3.local>
2017-06-03 19:50:47 -0400

    first

---
+++ script.js
@@ -0,0 +1,29 @@
+var muteLine = document.getElementById('muteLine');
+var mute = document.getElementById('muteButton');
+var audio = document.getElementById('audio');
+console.log(audio);
+
+
+var audio = document.getElementById("audio");
+var playing = false;
+
+mute.addEventListener('click', sound);
+
+
+
+
+function sound() {
+  console.log("click");
+  muteLine.classList.toggle('hidden');
+  if (playing) {
+    audio.pause();
+  } else {
+    audio.play();
+  }
+};
+audio.onplaying = function() {
+  playing = true;
+};
+audio.onpause = function() {
+  playing = false;
+};
